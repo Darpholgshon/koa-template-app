ef78f1f5118f2793f531b0c1d91dd810c1236ca9
Darpholgshon <ralph.hodgson@gmail.com>
2018-09-24 13:28:25 +0100

    Updated Info middleware

--- src/middleware/info.ts
+++ src/middleware/info.ts
@@ -5,19 +5,19 @@ import {ServerConfiguration} from '../util/server.configuration';
 
 const packageInfo = JSON.parse(fs.readFileSync(process.cwd() + '/package.json', 'utf-8'));
 
+const buildInfo = (ctx) => {
+  ctx.response.status = 200;
+  ctx.response.body = {
+    build: {
+      description: packageInfo.description,
+      version: process.env.VERSION || packageInfo.version,
+      name: packageInfo.name
+    }
+  };
+};
+
 const Info = (ctx, next) => {
-  if (ctx.url === `${ServerConfiguration.getContextPath()}/info`) {
-    ctx.response.status = 200;
-    ctx.response.body = {
-      build: {
-        description: packageInfo.description,
-        version: process.env.VERSION || packageInfo.version,
-        name: packageInfo.name
-      }
-    };
-    return;
-  }
-  return next();
+  return ctx.url === `${ServerConfiguration.getContextPath()}/info` ? buildInfo(ctx) : next();
 };
 
 export {Info};
