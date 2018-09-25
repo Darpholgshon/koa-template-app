5a7cb541355d300aa08bf9cbe22c54a576089146
Darpholgshon <ralph.hodgson@gmail.com>
2018-09-25 10:30:30 +0100

    Remove compiled code.

--- src/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-require("reflect-metadata");
-var routing_controllers_1 = require("routing-controllers");
-var app = routing_controllers_1.createKoaServer({
-    controllers: ['./controller/*.controller.ts']
-});
-app.listen(8888);
-//# sourceMappingURL=app.js.map
